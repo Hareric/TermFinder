98819941163096d35515de0cc13f0988d62fe8f9
Eric Chan <hareric@qq.com>
2017-06-08 22:23:05 +0800

    mutal information

--- create_feature.py
+++ create_feature.py
@@ -11,21 +11,20 @@ def mutual_information(text):
     计算每个字(词)相邻右边的互信息
     :return:
     """
-    text += " "
+    text = "".join(text.split())
+    text += "."
     # text = re.sub(u"[^\w\s]", u"", text)
     # text = re.sub(u"[()]", u"", text)
     count = np.zeros(len(text))
     cnt = np.zeros(len(text))
     fq = nltk.FreqDist(text)
+    bigrams_words = nltk.bigrams(text)
+    cfd = nltk.ConditionalFreqDist(bigrams_words)
     for i in range(len(text)-1):
         count[i] = fq[text[i]]
-        try:
-            cnt[i] = re.findall(text[i]+text[i+1], text).__len__()
-        except sre_constants.error:
-            print i
-            print text[i]+text[i+1]
-            cnt[i] = 1
-
+        cnt[i] = cfd[text[i]][text[i+1]]
+        if cnt[i] == 0:
+            print text[i], text[i+1]
     count[-1] = fq[text[-1]]
     ct = np.log(text.__len__()*cnt[:-1] / (count[:-1]*count[1:])) / np.log(2)
     # 离散化
@@ -51,17 +50,17 @@ def mutual_information(text):
 
 
 if __name__ == '__main__':
-    text = "你好吗a"
-    text = re.sub(u"[^\w\s]", u"", text)
-    text = re.sub(u"[()]", u"", text)
-    print text
+    # bigrams_words = nltk.bigrams(u"司XgI")
+    # for i in bigrams_words:
+    #     print i[0], i[1]
     t = codecs.open('/Users/Har/PycharmProjects/TermFinder/data/test_corpus/000159__2010_n.txt', 'r', 'utf-8').read()
+    # print t
+    mutual_information(t)
     # print t
     # ts = [i for i in t]
     # print ts.__len__()
     # mutual_information(ts)
 
-    mi(t)
     # a = np.array([1,2,3])
     # b = np.array([1,2,3])
     # print a[:-1]+b[1:]
